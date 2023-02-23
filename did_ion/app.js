9b762b4a3a8ab414ea0871f8f23f2f2b295b0441
Limbert Lino <limbertlino@gmail.com>
2023-02-22 21:38:42 -0400

    server created

---
+++ did_ion/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// app.use("/api/ion", require("./routes/index"));
+
+app.use(express.json());
+app.listen(PORT, () => {
+  console.log(`Server on ${PORT}`);
+});
